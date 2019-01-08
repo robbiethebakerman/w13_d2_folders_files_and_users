2bd82951aec53060465f2a8565a924244e114e76
Robbie Baker <robbiethebakerman@googlemail.com>
2019-01-08 20:47:03 +0000

    Adds DataLoader

--- src/main/java/com/codeclan/example/usersFileFoldersService/components/DataLoader.java
+++ src/main/java/com/codeclan/example/usersFileFoldersService/components/DataLoader.java
@@ -1,4 +1,43 @@
 package com.codeclan.example.usersFileFoldersService.components;
 
-public class DataLoader {
+import com.codeclan.example.usersFileFoldersService.models.File;
+import com.codeclan.example.usersFileFoldersService.models.Folder;
+import com.codeclan.example.usersFileFoldersService.models.User;
+import com.codeclan.example.usersFileFoldersService.repositories.FileRepository;
+import com.codeclan.example.usersFileFoldersService.repositories.FolderRepository;
+import com.codeclan.example.usersFileFoldersService.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    FolderRepository folderRepository;
+
+    @Autowired
+    FileRepository fileRepository;
+
+    public DataLoader() {
+
+    }
+
+    public void run(ApplicationArguments args) {
+
+        User user = new User("Donald_Trump");
+        userRepository.save(user);
+        Folder docs = new Folder("Docs", user);
+        folderRepository.save(docs);
+        Folder pics = new Folder("Pictures", user);
+        folderRepository.save(pics);
+        File taxReturns = new File("Tax Returns", "docx", 3.4, docs);
+        fileRepository.save(taxReturns);
+        File ivankaPic = new File("Picture of Ivanka on knee", "jpeg", 300, pics);
+        fileRepository.save(ivankaPic);
+    }
 }
