124273ffb3c17e76bc3470ab2203d1acee23627d
Robbie Baker <robbiethebakerman@googlemail.com>
2019-01-08 19:39:30 +0000

    Initial commit

---
+++ src/main/java/com/codeclan/example/usersFileFoldersService/UsersFileFoldersServiceApplication.java
@@ -0,0 +1,14 @@
+package com.codeclan.example.usersFileFoldersService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersFileFoldersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersFileFoldersServiceApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/codeclan/example/usersFileFoldersService/models/Folder.java
@@ -0,0 +1,4 @@
+package com.codeclan.example.usersFileFoldersService.models;
+
+public class Folder {
+}
